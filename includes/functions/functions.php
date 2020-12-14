ff47fb89355399c032cdf440081f8334131b9c67
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-12-14 21:27:24 +0100

    added https redirect

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -1,1 +1,8 @@
 <?php
+/**
+ * If https is used as protocol
+ * @return boolean
+ */
+function isHttps(){
+    return !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] !== "off";
+}
--- includes/includes.php
+++ includes/includes.php
@@ -6,6 +6,11 @@ require_once "/home/jaronz/config/config.php";
 require_once ROOT."/includes/db_connect.php";
 require_once ROOT."/includes/session_start.php";
 require_once ROOT."/includes/autoload.php";
+
+if(!LOCAL && !isHttps()){
+    header("location: https://".$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]);
+}
+
 require_once ROOT."/includes/vendor/autoload.php";
 $loader = new \Twig\Loader\FilesystemLoader(ROOT."/templates");
 $twig = new \Twig\Environment($loader, [
